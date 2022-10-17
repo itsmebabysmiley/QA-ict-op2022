7b20a2d0195999d5e3e4ce006998089d90b7e7f0
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-10-17 22:14:28 +0700

    change pnpm to yarn & init storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+}
+
+module.exports = config
