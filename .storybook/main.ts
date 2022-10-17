1c67ee12df1fcabad1131e2fb477ce5f460785c9
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-10-17 23:27:48 +0700

    add tailwindcss to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import { StorybookConfig } from '@storybook/react-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -7,10 +8,15 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-postcss',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  viteFinal: (config) => {
+    config.plugins = [...(config.plugins || []), tsconfigPaths()]
+    return config
   },
 }
 
