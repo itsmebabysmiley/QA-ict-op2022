c8e48dc855bae52ff4c00a6e5370b33bf73bd4ba
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-10-18 14:33:47 +0700

    fix args values

--- src/routes/Register/components/RegistrantTypeButton/RegistrantTypeButton.stories.tsx
+++ src/routes/Register/components/RegistrantTypeButton/RegistrantTypeButton.stories.tsx
@@ -28,41 +28,41 @@ const Template: ComponentStoryFn<typeof RegistrantTypeButton> = (args) => (
 export const Student = Template.bind({})
 Student.args = {
   label: 'Student',
-  icon: '/static/images/types/type-icon-student.svg',
+  icon: 'static/images/types/type-icon-student.svg',
   variant: 'primary',
 }
 
 export const UniversityStudent = Template.bind({})
 UniversityStudent.args = {
   label: 'University Student',
-  icon: '/static/images/types/type-icon-college-student.svg',
+  icon: 'static/images/types/type-icon-college-student.svg',
   variant: 'secondary',
 }
 
 export const Teacher = Template.bind({})
 Teacher.args = {
   label: 'Teacher',
-  icon: '/static/images/types/type-icon-teacher.svg',
+  icon: 'static/images/types/type-icon-teacher.svg',
   variant: 'primary',
 }
 
 export const Parents = Template.bind({})
 Parents.args = {
   label: 'Parents',
-  icon: '/static/images/types/type-icon-parents.svg',
+  icon: 'static/images/types/type-icon-parents.svg',
   variant: 'secondary',
 }
 
 export const MuStaff = Template.bind({})
 MuStaff.args = {
   label: 'MU Staff',
-  icon: '/static/images/types/type-icon-mu-staff.svg',
+  icon: 'static/images/types/type-icon-mu-staff.svg',
   variant: 'primary',
 }
 
 export const Others = Template.bind({})
 Others.args = {
   label: 'MU Staff',
-  icon: '/static/images/types/type-icon-external.svg',
+  icon: 'static/images/types/type-icon-external.svg',
   variant: 'secondary',
 }
