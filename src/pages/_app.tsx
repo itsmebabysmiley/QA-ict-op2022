a8b42a51ac885f3a5e63ef10494915ae5a53ac15
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-10-20 11:41:46 +0700

    add liff provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '~/utils/iconify.ts'
+import { LiffProvider } from 'react-liff'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <LiffProvider liffId={process.env.NEXT_PUBLIC_LIFF_ID ?? ''}>
+        <Component {...pageProps} />
+      </LiffProvider>
+    </>
+  )
 }
 
 export default App
