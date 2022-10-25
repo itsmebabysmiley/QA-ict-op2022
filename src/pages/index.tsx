c8d56ab1a65738dd8dc8e77afb3b1adfc7172409
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-10-26 01:08:31 +0700

    fix typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Page: NextPage = () => {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
-      <h1>ICT Mahidol Openhouase</h1>
+      <h1 className="font-heading font-bold">ICT Mahidol Open House</h1>
     </div>
   )
 }
