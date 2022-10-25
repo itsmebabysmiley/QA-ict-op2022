8886eb89afc164c3cc3e5f0368350de5ebe78842
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-10-26 01:01:34 +0700

    test ci

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const Page: NextPage = () => {
-  return <div></div>
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <h1>ICT Mahidol Openhouase</h1>
+    </div>
+  )
 }
 
 export default Page
