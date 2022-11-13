cd3b056f86d239431eaeb3bb35f5a6729aa984c1
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-11-13 13:35:17 +0700

    modified test suite

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -5,5 +5,7 @@ import { fireEvent, render, screen } from "@testing-library/react";
 describe("Test Test", () => {
     it("Is it working", () => {
       render(<Page />);
+      // Screen contain 'hello'
+      screen.getByText('hello');
     });
   });
