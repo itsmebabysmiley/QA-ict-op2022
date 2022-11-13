ae8754a0a5a9d446383cdfba73c7ea5fdcbf073b
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-11-13 13:14:19 +0700

    added first test suite

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,9 @@
+import Page from "../src/pages/index";
+import "@testing-library/jest-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("Test Test", () => {
+    it("Is it working", () => {
+      render(<Page />);
+    });
+  });
